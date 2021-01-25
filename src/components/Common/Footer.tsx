3585820f57131712a2377d976fb9d4e4293cd02a
ji5485 <dong5485@gmail.com>
2021-01-25 14:57:21 +0000

    Develop Main Page

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+
+const Header: FunctionComponent = function () {
+  return <div></div>;
+};
+
+export default Header;
