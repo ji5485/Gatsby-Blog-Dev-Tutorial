cc6762c992633ffbc7a129f38f2bb9218e9976e9
ji5485 <dong5485@gmail.com>
2021-01-26 14:58:08 +0000

    Developing Main Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
+import GlobalStyle from 'components/Common/GlobalStyle';
 import Introduction from 'components/Main/Introduction';
 
 const Container = styled.div`
-  width: 768px;
-  margin: 0 auto;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 `;
 
 const IndexPage: FunctionComponent = function () {
   return (
     <Container>
+      <GlobalStyle />
+
       <Introduction />
     </Container>
   );
