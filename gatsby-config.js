699dbfc314ddf75ec0f6bef52f0f41c1fb6da121
ji5485 <dong5485@gmail.com>
2021-02-21 18:03:51 +0000

    Modify Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `주니어 개발자의 개발 블로그`,
     description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
     author: `Hyun`,
-    url: 'https://my-website-link.com',
+    siteUrl: 'https://my-website-link.com',
   },
   plugins: [
     {
@@ -68,6 +68,13 @@ module.exports = {
             },
           },
         ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://my-website.com/',
+        stripQueryString: true,
       },
     },
   ],
