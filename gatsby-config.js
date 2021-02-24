2b5599d72742f7b53bdf31f6b3c3480a667d4562
ji5485 <dong5485@gmail.com>
2021-02-24 08:36:51 +0000

    Modify Canonical URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `주니어 개발자의 개발 블로그`,
     description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
     author: `Hyun`,
-    siteUrl: 'https://hd152534.github.io',
+    siteUrl: 'https://hd152534.github.io/',
   },
   plugins: [
     {
@@ -73,7 +73,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://my-website.com/',
+        siteUrl: 'https://hd152534.github.io/',
         stripQueryString: true,
       },
     },
